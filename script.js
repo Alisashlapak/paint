a2945b407e00a1952758bf6b15609cee4db18ded
Alisiia <alisaslapak1@gmail.com>
2025-02-05 01:03:50 +0200

    undo/redo

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ const brushSize = document.getElementById('brush-size');
 const colorPicker = document.getElementById('color-picker');
 const clearAll = document.getElementById('clear');
 const save = document.getElementById('save');
- let isDrawing = false;
+const undo = document.getElementById('undo');
+const redo = document.getElementById('redo');
+
+let isDrawing = false;
+let history = [];
+let redoStack = [];
 
 canvas.width = window.innerWidth - 40;
 canvas.height = window.innerHeight * 0.85;
@@ -13,15 +18,17 @@ ctx.lineWidth = 5;
 ctx.lineCap = 'round';
 ctx.strokeStyle = 'black';
 
+
 function startPosition(e){
     isDrawing = true;
     ctx.beginPath();
     ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
-    }
+    saveState(); }
 
 
 function endPosition(){
     isDrawing = false;
+
    } 
 
 function draw(e){
@@ -38,6 +45,7 @@ function draw(e){
         e.clientX - canvas.offsetLeft,
         e.clientY - canvas.offsetTop
     );
+    saveState();
 }
 
 canvas.addEventListener('mousedown', startPosition);
@@ -45,16 +53,18 @@ canvas.addEventListener('mouseup', endPosition);
 canvas.addEventListener('mousemove', draw);
 clearAll.onclick=()=>{
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    saveState();
 };
 brushSize.addEventListener('input', () => {
     ctx.lineWidth = parseInt(brushSize.value, 10);
     updateBrushSizeLabel(brushSize.value);
+    saveState();
 });
 
 function updateBrushSizeLabel (size){
     const brushSizeLabel = document.getElementById('brush-size-label');
     if (brushSizeLabel){
-        brushSizeLabel.textContent = `Brush Size: ${size}`;
+        brushSizeLabel.textContent = `Thickness: ${size}`;
     }
 }
 const pen = document.getElementById('pen');
@@ -79,3 +89,30 @@ save.onclick = () => {
     link.download = 'drawing.png';
     link.click();
 };
+function saveState() {
+    history.push(canvas.toDataURL());
+    redoStack = [];}
+
+undo.onclick = () =>{
+    if(history.length > 0){
+        redoStack.push(canvas.toDataURL());
+        let previousState = history.pop();
+        let img = new Image();
+        img.src = previousState;
+        img.onload = () =>{
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(img, 0, 0);
+        }
+    }
+}
+redo.onclick = () => {
+    if (redoStack.length > 0) {
+        let redoState = redoStack.pop();
+        history.push(canvas.toDataURL());
+        let img = new Image();
+        img.src = redoState;
+        img.onload = () => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(img, 0, 0);
+        };
+    }}
